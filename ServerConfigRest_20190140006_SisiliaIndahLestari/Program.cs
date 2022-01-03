06d17852bdaa780508ebdb7917fe6e7138ed6c1d
Sisilia Indah <sisilia.indah.ft19@mail.umy.ac.id>
2022-01-03 10:18:14 +0700

    Add project files.

---
+++ ServerConfigRest_20190140006_SisiliaIndahLestari/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20190140006_SisiliaIndahLestari;
+
+namespace ServerConfigRest_20190140006_SisiliaIndahLestari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
